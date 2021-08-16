84cbc3ee8b01385ad7fe3dadf1bf735bc69624cf
eze-higa <53649465+eze-higa@users.noreply.github.com>
2021-08-16 12:29:37 -0300

    Create NoteRepository for interface and implementation

---
+++ dapper-demo/dapper-demo/Repositories/INoteRepository.cs
@@ -0,0 +1,13 @@
+﻿using dapper_demo.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dapper_demo.Repositories
+{
+    public interface INoteRepository
+    {
+        Task<IEnumerable<Note>> GetNotes();
+    }
+}
---
+++ dapper-demo/dapper-demo/Repositories/NoteRepository.cs
@@ -0,0 +1,29 @@
+﻿using Dapper;
+using dapper_demo.Context;
+using dapper_demo.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dapper_demo.Repositories
+{
+    public class NoteRepository : INoteRepository
+    {
+        private readonly IDapperContext dapperContext;
+        public NoteRepository(IDapperContext dapperContext)
+        {
+            this.dapperContext = dapperContext;
+        }
+        public async Task<IEnumerable<Note>> GetNotes()
+        {
+            List<Note> notes = new List<Note>();
+            var query = "SELECT * FROM Notes";
+            using (var connection = dapperContext.CreateConnection())
+            {
+                notes = (await connection.QueryAsync<Note>(query)).ToList();
+            }
+            return notes;
+        }
+    }
+}

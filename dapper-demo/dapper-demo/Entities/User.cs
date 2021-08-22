c1620de36a5299926d9cae3fd5fb9d737e613861
eze-higa <53649465+eze-higa@users.noreply.github.com>
2021-08-22 11:58:56 -0300

    Create User.cs
    Create User entity

---
+++ dapper-demo/dapper-demo/Entities/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dapper_demo.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public List<Note> Notes { get; set; }
+        public User()
+        {
+            this.Notes = new List<Note>();
+        }
+    }
+}

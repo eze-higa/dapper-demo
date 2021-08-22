6e17c94d6b92d6f8f282a61b095c3689b213e5df
eze-higa <53649465+eze-higa@users.noreply.github.com>
2021-08-22 11:59:11 -0300

    Update Note.cs
    Add User Entity to Note

--- dapper-demo/dapper-demo/Entities/Note.cs
+++ dapper-demo/dapper-demo/Entities/Note.cs
@@ -10,6 +10,7 @@ namespace dapper_demo.Entities
         public int Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
+        public User User { get; set; }
         public Note()
         {
 

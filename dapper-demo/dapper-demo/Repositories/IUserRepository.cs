a012538e177d180e4692dea257a594e28f4f308c
eze-higa <53649465+eze-higa@users.noreply.github.com>
2021-08-22 11:59:26 -0300

    Create IUserRepository.cs
    Create interface for user repository

---
+++ dapper-demo/dapper-demo/Repositories/IUserRepository.cs
@@ -0,0 +1,16 @@
+﻿using dapper_demo.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dapper_demo.Repositories
+{
+    public interface IUserRepository
+    {
+        Task<IList<User>> GetUsers();
+        Task<User> GetUserById(int id);
+        Task<User> GetUserByUsername(string username);
+        Task Create(string username);
+    }
+}

533371d452704f4f6932054b23d65a86a6549af2
eze-higa <53649465+eze-higa@users.noreply.github.com>
2021-09-26 10:19:12 -0300

    Create NoteProfile.cs
    Create Profile for Note

---
+++ dapper-demo/dapper-demo/Configurations/NoteProfile.cs
@@ -0,0 +1,17 @@
+﻿
+
+using AutoMapper;
+using dapper_demo.DTOs.NoteDTOs;
+using dapper_demo.Entities;
+
+namespace dapper_demo.Configurations
+{
+    public class NoteProfile: Profile
+    {
+        public NoteProfile()
+        {
+            CreateMap<NoteWriteDTO, Note>();
+            CreateMap<Note, NoteReadDTO>();
+        }
+    }
+}

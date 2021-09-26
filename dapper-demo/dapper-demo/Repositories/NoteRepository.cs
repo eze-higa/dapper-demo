22b929154e6d57c13171d669960fd9d449fdf92a
eze-higa <53649465+eze-higa@users.noreply.github.com>
2021-09-26 11:29:03 -0300

    Update NoteRepository.cs
    Modification of Create method, returns the created note with the Id.

--- dapper-demo/dapper-demo/Repositories/NoteRepository.cs
+++ dapper-demo/dapper-demo/Repositories/NoteRepository.cs
@@ -1,7 +1,6 @@
 ﻿using Dapper;
 using dapper_demo.Context;
 using dapper_demo.Entities;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -16,21 +15,36 @@ namespace dapper_demo.Repositories
             this._dapperContext = dapperContext;
         }
 
-        public async Task Create(Note note)
+        public async Task<Note> Create(Note note)
         {
-            string query = "INSERT INTO Notes (Title, Description, UserID) VALUES (@title, @description, @userId)";
-
+            string query = @"INSERT INTO Notes (Title, Description, UserID) VALUES (@title, @description, @userId)
+                            SELECT CAST(SCOPE_IDENTITY() AS int)";            
             using(var connection = _dapperContext.CreateConnection())
             {
-                await connection.ExecuteAsync(query, new { title = note.Title, description = note.Description, userId = note.UserID });
+                note.Id = await connection.QuerySingleAsync<int>(query, new { title = note.Title, description = note.Description, userId = note.UserID });
             }
+            return note;
+        }
+
+        public async Task<Note> GetNoteById(int id)
+        {
+            Note note = null;
+
+            string query = "SELECT * FROM Notes WHERE Id = @noteId";
+            
+            using (var connection = _dapperContext.CreateConnection())
+            {
+                note = (await connection.QueryFirstOrDefaultAsync<Note>(query, new { noteId = id }));
+            }
+
+            return note;
         }
 
         public async Task<IEnumerable<Note>> GetNotes()
         {
             List<Note> notes = new List<Note>();
             var query = @"SELECT * FROM Notes n 
-                            INNER JOIN Users u ON n.UserID = u.Id";
+                        INNER JOIN Users u ON n.UserID = u.Id";
             using (var connection = _dapperContext.CreateConnection())
             {
                 notes = (await connection.QueryAsync<Note, User, Note>(

6eb640850953b534fc97ac670cfd6cf700e7a9d6
eze-higa <53649465+eze-higa@users.noreply.github.com>
2021-10-11 11:41:03 -0300

    Create UserProfile.cs
    Add profile for mapping

---
+++ dapper-demo/dapper-demo/Configurations/UserProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using dapper_demo.DTOs.UserDTOs;
+using dapper_demo.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dapper_demo.Configurations
+{
+    public class UserProfile: Profile
+    {
+        public UserProfile()
+        {
+            CreateMap<UserWriteDTO, User>();
+            CreateMap<User, UserReadDTO>();
+        }
+    }
+}

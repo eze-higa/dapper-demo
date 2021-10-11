6129fcee27290eb490428648ff5076f6f7709d97
eze-higa <53649465+eze-higa@users.noreply.github.com>
2021-10-11 11:42:19 -0300

    Update UsersController.cs
    Replace User with DTOs

--- dapper-demo/dapper-demo/Controllers/UsersController.cs
+++ dapper-demo/dapper-demo/Controllers/UsersController.cs
@@ -1,4 +1,6 @@
-﻿using dapper_demo.Entities;
+﻿using AutoMapper;
+using dapper_demo.DTOs.UserDTOs;
+using dapper_demo.Entities;
 using dapper_demo.Repositories;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -16,31 +18,51 @@ namespace dapper_demo.Controllers
     {
         private readonly ILogger<UsersController> _logger;
         private readonly IUserRepository _userRepository;
-        public UsersController(ILogger<UsersController> logger, IUserRepository userRepository)
+        private readonly IMapper _mapper;
+        public UsersController(ILogger<UsersController> logger, IUserRepository userRepository, IMapper mapper)
         {
             this._logger = logger;
             this._userRepository = userRepository;
+            this._mapper = mapper;
         }
 
         [HttpGet]        
-        [ProducesResponseType(typeof(IList<User>), StatusCodes.Status200OK)]
-        public async Task<IActionResult> Get() => Ok(await this._userRepository.GetUsers());
+        [ProducesResponseType(typeof(IList<UserReadDTO>), StatusCodes.Status200OK)]
+        public async Task<IActionResult> Get() => Ok(_mapper.Map<List<UserReadDTO>> (await this._userRepository.GetUsers()));
 
         [HttpGet("id/{id}")]
-        [ProducesResponseType(typeof(User), StatusCodes.Status200OK)]
-        public async Task<IActionResult> GetById(int id) => Ok(await _userRepository.GetUserById(id));
+        [ProducesResponseType(typeof(UserReadDTO), StatusCodes.Status200OK)]
+        public async Task<IActionResult> GetById(int id) {
+            User user = await _userRepository.GetUserById(id);
+
+            if(user == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(_mapper.Map<UserReadDTO>(user));
+        }
 
         [HttpGet("username/{username}")]
-        [ProducesResponseType(typeof(User), StatusCodes.Status200OK)]
-        public async Task<IActionResult> GetByUsername(string username) => Ok(await _userRepository.GetUserByUsername(username));
+        [ProducesResponseType(typeof(UserReadDTO), StatusCodes.Status200OK)]
+        public async Task<IActionResult> GetByUsername(string username) {
+            User user = await _userRepository.GetUserByUsername(username);
+
+            if(user == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(_mapper.Map<UserReadDTO>(user));
+        }
 
         [HttpPost]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public async Task<IActionResult> Post([FromBody] string username) {
-            if (string.IsNullOrEmpty(username))
+        public async Task<IActionResult> Post([FromBody] UserWriteDTO userWriteDTO) {
+            if (string.IsNullOrEmpty(userWriteDTO.Username))
                 return BadRequest("Username is required.");
-            await _userRepository.Create(username);
+            await _userRepository.Create(_mapper.Map<User>(userWriteDTO));
             return Ok();
         }
     }

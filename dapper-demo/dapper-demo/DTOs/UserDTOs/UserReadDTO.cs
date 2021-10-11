f725574a09562fd0d49d447a02223eb78bfcc443
eze-higa <53649465+eze-higa@users.noreply.github.com>
2021-10-11 11:40:50 -0300

    Add UserDTOs
    Add UserDTO for reading and writting

---
+++ dapper-demo/dapper-demo/DTOs/UserDTOs/UserReadDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dapper_demo.DTOs.UserDTOs
+{
+    public record UserReadDTO
+    {
+        public int Id { get; init; }
+        public string Username { get; init; }
+    }
+}
---
+++ dapper-demo/dapper-demo/DTOs/UserDTOs/UserWriteDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dapper_demo.DTOs.UserDTOs
+{
+    public record UserWriteDTO
+    {
+        public string Username { get; init; }
+    }
+}
